1c3ba4ef9ef40ed27b94c9faaacd4546bf455b96
LYNETTE WAMBUI <lynettemuraga47@gmail.com>
2025-07-05 17:13:24 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+print("✋ Hi there! Let's learn about Time Complexity.")
+
+# Ask the user for a number
+n = int(input("Enter a number: "))
+
+print("\n I'm going to count from 1 to your number:")
+for i in range(1, n+1):
+    print("⏱️⏱️ Step {1}: Counting...")
+
+print("\n ✅ Done! That took me", n, "steps.")
